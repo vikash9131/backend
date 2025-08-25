fc31a038b72ca624bb44651920339a62e84cfea2
Vikash Kumar <vikashkumar3165676@gmail.com>
2025-08-25 15:37:32 +0530

    new commikt

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config()
+
+import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error", (error) =>{
+            console.log("Error:",error);
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error",error)
+        throw error
+    }
+})()
+*/    
